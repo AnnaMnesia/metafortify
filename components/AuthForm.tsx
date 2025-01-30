2b023d109ab90326e8b06d36a636b3ffcd5e764e
Anna Mastoridou <annamastoridou@Annas-Mac-mini.local>
2025-01-30 12:38:31 +0100

    added auth OTP

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -150,7 +150,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
       {/* OTP Verification */}
-      {true && (
+      {accountId && (
         <OTPModal email={form.getValues("email")} accountId={accountId} />
       )}
     </>
