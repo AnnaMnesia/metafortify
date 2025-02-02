b5f7849b9687a463fc56a766e63578ef3002df8b
Anna Mastoridou <annamastoridou@Annas-Mac-mini.local>
2025-02-02 12:00:26 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,8 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
 
+export const dynamic = "force-dynamic";
+
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
   if (!currentUser) return redirect("/sign-in");
@@ -15,10 +17,10 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} />
 
-        <Header userId={currentUser.$id} accountId={currentUser.accountId}/>
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
-      <Toaster/>
+      <Toaster />
     </main>
   );
 };
